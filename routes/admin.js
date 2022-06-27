4ca797974694d28596295f474a183e23c7f197c0
fabio-sv <fabio_dev@outlook.com>
2022-06-27 20:37:40 -0300

    update

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Principal");
+    res.render("admin/index");
 });
 
-router.get("/post", (req, res) => {
-    res.send("Posts");
+router.get("/posts", (req, res) => {
+    res.render("admincategorias");
+});
+
+router.get("/categorias", (req, res) => {
+    res.render("admin/categorias");
+});
+
+router.get("/categorias/add", (req, res) => {
+    res.render("admin/addcategorias");
 });
 
 module.exports = router;
